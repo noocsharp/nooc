b1e6dd30f644c4cb3f1bdcd9b2cfd254b7d885b8
Nihal Jere <nihal@nihaljere.xyz>
2021-11-18 14:44:00 -0600

    array: determine item size properly

--- array.h
+++ array.h
@@ -2,6 +2,6 @@
 	_array_add((void **) &(arr->data), sizeof(new), 1, &(arr->len), &(arr->cap), &new);
 
 #define array_push(arr, new, count) \
-	_array_add((void **) &(arr->data), sizeof(*new), count, &(arr->len), &(arr->cap), new);
+	_array_add((void **) &(arr->data), sizeof(*(arr->data)), count, &(arr->len), &(arr->cap), new);
 
 int _array_add(void **data, size_t size, size_t count, size_t *len, size_t *cap, void *new);
